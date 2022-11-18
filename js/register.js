fe33ea48c0ee544af048c0db26ea9f02bd9e99b2
sridevi14 <sridevimanjuraja@gmail.com>
2022-11-18 13:59:08 +0530

    confirm password

--- js/register.js
+++ js/register.js
@@ -6,29 +6,31 @@ if (session_Id) {
 
 $(document).ready(function () {
   $("#myForm").submit(function (e) {
-    var pw1 = document.getElementById("password");
-    var pw2 = document.getElementById("confirm_password");
-    // if (pw1 != pw2 && pw1 != "") {
-    //   alert("Passwords did not match");
-    // } else {
     e.preventDefault();
-    var page = "./login.html";
+    var pw1 = document.getElementById("password").value;
+    var pw2 = document.getElementById("confirm_password").value;
+    if (pw1 && pw1 != pw2) {
+      alert("Passwords did not match");
+      return;
+    } else {
+      e.preventDefault();
+      var page = "./login.html";
 
-    $.ajax({
-      url: "./php/register.php?",
-      method: "POST",
-      data: $("#myForm").serialize(),
-      success: function (res) {
-        if (res == 2) {
-          $("#register_output").html("Registered Successfully");
+      $.ajax({
+        url: "./php/register.php?",
+        method: "POST",
+        data: $("#myForm").serialize(),
+        success: function (res) {
+          if (res == 2) {
+            $("#register_output").html("Registered Successfully");
 
-          window.location.href = page;
-        }
-        if (res == 1) {
-          $("#register_output").html(" Already Exist");
-        }
-      },
-    });
-    //}
+            window.location.href = page;
+          }
+          if (res == 1) {
+            $("#register_output").html(" Already Exist");
+          }
+        },
+      });
+    }
   });
 });
